ffcaeb5b6760defa706675c7c594539a72675ffd
bradcoomer-boop <bradcoomer@gmail.com>
2026-03-31 23:59:54 +0100

    Create src/App.jsx file with App component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to the Japan Trip Planner!</h1>
+            <p>Plan your perfect trip to Japan with ease.</p>
+        </div>
+    );
+};
+
+export default App;
